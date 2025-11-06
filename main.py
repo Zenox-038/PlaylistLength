d03db1384e4cd86fa4649bad88bbd51072ea2bf1
Zenox-038 <reach2rafiya@gmail.com>
2025-11-06 22:16:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿from bs4 import BeautifulSoup
+import requests
+
+
+url=input('Enter Playlist URL:')
+if not url.startswith('https://www.youtube.com/playlist?list='):
+    print('Invalid URL. Please enter a valid YouTube playlist URL.')
+    exit()
+if requests.get(url).status_code==200:
+    with open(url, 'r', encoding='utf-8') as file:
+        soup=BeautifulSoup(file, 'html.parser')
